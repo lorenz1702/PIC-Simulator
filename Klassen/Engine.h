3c8e3f0bb31f3ec9f73df6003ab4fc697f476fed
xdaWolf <sarah.holzmann@gmx.net>
2023-04-17 14:32:29 +0200

    17.4

---
+++ Klassen/Engine.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/Graphics.hpp>
+
+#ifndef Engine_H_
+#define Engine_H_
+
+class Engine
+{
+    private:
+    int Datenspeicher[1024]; //oberen 2 Bits sind immer 0, ggf. verunden
+
+    public:
+
+
+} ;
+
+#endif
